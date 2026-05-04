4759f38909b28166703ead5129e6d2c2a4c8aca5
Rakhi <isainirakhi@gmail.com>
2026-05-04 11:25:47 +0530

    Update Heart.jsx

--- src/Components/Heart.jsx
+++ src/Components/Heart.jsx
@@ -1,23 +1,3 @@
-// import { useState } from 'react'
-
-// function Heart(){
-// const HeartToggle =()=>{
-//   const[isLiked , setLiked]=useState(false);
-//      const toggleLike =()=>{
-//       setLiked(!Liked);
-
-//      }
-// return(
-//     <div>
-//   <button onClick={toggleLike}>
-//     {isLiked? "🤍": "❤️"}
-//   </button>
-//   </div>
-// )
-// };
-// }
-// export default Heart;
-
 import { useState } from 'react'
 
 function Heart(){
